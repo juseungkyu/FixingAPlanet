d835dc3ca0e2d0e15035b5297901a8e7a20c940e
kyuseungkyuju <juseungkyu0711@gmail.com>
2022-10-28 12:57:36 +0900

    워크스페이스 설정

--- PlanetEditor/WebContent/resources/js/util/util.js
+++ PlanetEditor/WebContent/resources/js/util/util.js
@@ -21,40 +21,6 @@ function getRadian(degrees) {
 }
 
 /**
- * 두 공간 벡터의 외적 계산
- * @param {Array} a 
- * @param {Array} b 
- * @returns 
- */
-function cross(a, b) {
-    return [
-        a[1] * b[2] - a[2] * b[1],
-        a[2] * b[0] - a[0] * b[2],
-        a[0] * b[1] - a[1] * b[0]
-    ];
-}
-
-/**
- * 두 공간 벡터의 차
- * @param {Array} a 
- * @param {Array} b 
- * @returns 
- */
-function vectorSubtract(a, b) {
-    return [a[0] - b[0], a[1] - b[1], a[2] - b[2]];
-}
-
-/**
- * 정규화 (단위 백터로 변환)
- * @param {Array} v 
- * @returns 
- */
-function normalize(v) {
-    const length = Math.sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
-    return length > 0.001 ? [v[0] / length, v[1] / length, v[2] / length] : [0,0,0]
-}
-
-/**
  * 서로 다른 두 좌표 사이의 각도
  * @param {Number} x1
  * @param {Number} y1
