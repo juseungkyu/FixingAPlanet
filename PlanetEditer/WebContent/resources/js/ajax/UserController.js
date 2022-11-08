f2802fea19bf4ef42b81fd1f554cf85179ac9a6d
kyuseungkyuju <juseungkyu0711@gmail.com>
2022-11-08 12:00:08 +0900

    session

--- PlanetEditer/WebContent/resources/js/ajax/UserController.js
+++ PlanetEditer/WebContent/resources/js/ajax/UserController.js
@@ -2,6 +2,74 @@ import Controller from "./Controller";
 
 export default class UserController extends Controller{
     constructor() {
-        
+        super()
     }
+    
+    /**
+     * 유저 정보를 반환
+     * @param {String} id 
+     * @param {String} pw 
+     * 
+     * @returns  {
+     *    'error' = true
+     *    'data' : "id가 누락 되었습니다. 등 오류 메시지"
+     *  }
+     * @returns  {
+     *    'error' = false
+     *    'data' : {
+     *         "loginSuccess" : false,
+     *         "user" : null
+     *     }
+     *  }
+     * @returns {
+     *    'error' = false
+     *    'data' : {
+     *         "loginSuccess" : true 
+     *         "user" : {
+     *              userId : String,
+     *              userName : String,          
+     *         }
+     *     }
+     *  }
+     */
+    login = async (id, pw) => await this.post('/user/login', {id, pw})
+
+    /**
+     * 유저 정보를 반환
+     * 
+     * @returns  {
+     *    'error' = true
+     *    'data' : "로그아웃을 실패했습니다."
+     *  }
+     * @returns  {
+     *    'error' = false
+     *    'data' : {
+     *         "message" : "로그아웃 성공"
+     *     }
+     *  }
+     */
+    logout = async () => await this.get('/user/logout')
+
+
+    /**
+     * 유저 정보를 반환
+     * 
+     * @param {String} id 
+     * @param {String} pw 
+     * @param {String} name 
+     * 
+     * @returns  {
+     *    'error' = true
+     *    'data' : "id가 누락 되었습니다. 등 오류 메시지"
+     * }
+     * @returns {
+     *    'error' = false
+     *    'data' : {
+     *         "loginSuccess" : true
+     *     }
+     * }
+     */
+    join = async (id, pw, name) => await this.post('/user/logout', {id, pw})
 }
+
+new UserController().login('123', '1234')
